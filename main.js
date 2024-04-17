5522c594bbc6b2475c35b299a238a866551dc16e
Samudra-Ekanayake <samudra2724@gmail.com>
2024-04-17 18:24:59 +0200

    prezzo biglietto

---
+++ main.js
@@ -0,0 +1,7 @@
+let kmpercorso = prompt ("inserisci quanti km vuoi percorrere")
+
+let prezzokm = 2;
+
+let prezzobiglietto = kmpercorso * prezzokm;
+
+console.log("il prezzo del biglietto è " + prezzobiglietto)
